a80750e5e22c5d107c46dc195481e6fcb2bb06e9
Your Name <you@example.com>
2022-04-04 10:36:26 +0530

    Second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,3 @@
 // ignore_for_file: prefer_const_constructors
 
-import 'package:flutter/material.dart';
 
-import 'Screen/login_page.dart';
-
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Hello There",
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: LoginPage(),
-    );
-  }
-}
